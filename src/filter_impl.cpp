932ab21dda90925b153ef48e37e231c330608ce9
Denis STOJILJKOVIC <denis.stojiljkovic@epita.fr>
2023-11-30 12:06:03 +0100

    Fixed CPP Implementation, added Refresh Background Median

--- src/filter_impl.cpp
+++ src/filter_impl.cpp
@@ -18,77 +18,89 @@ struct CIELAB {
     double L, a, b;
 };
 
-XYZ rgb_to_XYZ(rgb rgb) {
-    double r = rgb.r / 255.0;
-    double g = rgb.g / 255.0;
-    double b = rgb.b / 255.0;
+std::vector<double> createCorrectColorLUT() {
+    std::vector<double> lut(256);
+    for (int i = 0; i < 256; ++i) {
+        double c = i / 255.0;
+        lut[i] = c > 0.04045 ? pow((c + 0.055) / 1.055, 2.4) : c / 12.92;
+    }
+    return lut;
+}
 
-    r = r > 0.04045 ? pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
-    g = g > 0.04045 ? pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
-    b = b > 0.04045 ? pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+inline XYZ rgb_to_XYZ(rgb rgb) {
+    static const std::vector<double> correctColorLUT = createCorrectColorLUT();
 
-    r *= 100.0;
-    g *= 100.0;
-    b *= 100.0;
+    double r = correctColorLUT[rgb.r] * 100.0;
+    double g = correctColorLUT[rgb.g] * 100.0;
+    double b = correctColorLUT[rgb.b] * 100.0;
 
-    XYZ xyz;
-    xyz.X = r * 0.4124 + g * 0.3576 + b * 0.1805;
-    xyz.Y = r * 0.2126 + g * 0.7152 + b * 0.0722;
-    xyz.Z = r * 0.0193 + g * 0.1192 + b * 0.9505;
+    // Direct computation
+    XYZ xyz = {
+            r * 0.4124 + g * 0.3576 + b * 0.1805,
+            r * 0.2126 + g * 0.7152 + b * 0.0722,
+            r * 0.0193 + g * 0.1192 + b * 0.9505
+    };
 
     return xyz;
 }
 
-CIELAB xyz_to_CIELAB(XYZ xyz) {
+inline CIELAB xyz_to_CIELAB(XYZ xyz) {
+    // Precompute division
     double x = xyz.X / 95.047;
-    double y = xyz.Y / 100.000;
+    double y = xyz.Y / 100.0;
     double z = xyz.Z / 108.883;
 
-    x = x > 0.008856 ? pow(x, 1.0/3.0) : (7.787 * x) + (16.0 / 116.0);
-    y = y > 0.008856 ? pow(y, 1.0/3.0) : (7.787 * y) + (16.0 / 116.0);
-    z = z > 0.008856 ? pow(z, 1.0/3.0) : (7.787 * z) + (16.0 / 116.0);
+    // Using a lambda for the repeated operation
+    auto f = [](double t) -> double {
+        return t > 0.008856 ? pow(t, 1.0 / 3.0) : (7.787 * t) + (16.0 / 116.0);
+    };
 
-    CIELAB lab;
-    lab.L = (116.0 * y) - 16.0;
-    lab.a = 500.0 * (x - y);
-    lab.b = 200.0 * (y - z);
+    x = f(x);
+    y = f(y);
+    z = f(z);
+
+    // Direct computation
+    CIELAB lab = {
+            (116.0 * y) - 16.0,
+            500.0 * (x - y),
+            200.0 * (y - z)
+    };
 
     return lab;
 }
 
-CIELAB rgb_to_CIELAB(rgb rgb) {
+inline CIELAB rgb_to_CIELAB(rgb rgb) {
     XYZ xyz = rgb_to_XYZ(rgb);
     return xyz_to_CIELAB(xyz);
 }
 
-CIELAB* convert_image_to_lab(rgb* rgb_image, int width, int height) {
-    CIELAB* cielab = (CIELAB*) malloc(width * height * sizeof(CIELAB));
-    for (int y = 0; y < height; ++y)
-    {
-        rgb* lineptr = (rgb*) (rgb_image + y * width);
-        for (int x = 0; x < width; ++x)
-        {
-            cielab[y*width + x] = rgb_to_CIELAB(lineptr[x]);
-        }
+inline CIELAB* convert_image_to_lab(const rgb* rgb_image, int width, int height) {
+    CIELAB* cielab = new CIELAB[width * height];
+    for (int i = 0; i < width * height; ++i) {
+        cielab[i] = rgb_to_CIELAB(rgb_image[i]);
     }
     return cielab;
 }
 
 double* compute_residual_image(CIELAB* background, CIELAB* image, int width, int height) {
-    double* residual_image = new double[height*width];
-    for (int y = 0; y < height; ++y)
-    {
+    double* residual_image = new double[height * width];
+    for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
-            residual_image[y*width + x] = sqrt(pow(image[y*width + x].L - background[y*width + x].L, 2) + pow(image[y*width + x].a - background[y*width + x].a, 2) + pow(image[y*width + x].b - background[y*width + x].b, 2));
+            int idx = y * width + x;
+            double dL = image[idx].L - background[idx].L;
+            double da = image[idx].a - background[idx].a;
+            double db = image[idx].b - background[idx].b;
+            residual_image[idx] = sqrt(dL * dL + da * da + db * db);
         }
     }
     return residual_image;
 }
 
-double* errosion_dilatation(double* residual_image, int width, int height, int radius) {
+double* erosion_dilatation(double* residual_image, int width, int height, int radius) {
     double* temp_image = new double[width * height];
     double* output_image = new double[width * height];
 
+    // Erosion
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
             double min_val = std::numeric_limits<double>::max();
@@ -99,10 +111,10 @@ double* errosion_dilatation(double* residual_image, int width, int height, int r
                         int nx1 = x + dx;
                         int nx2 = nx1 + 1;
                         if (nx1 >= 0 && nx1 < width) {
-                            min_val = std::min(min_val, image[ny * width + nx1]);
+                            min_val = std::min(min_val, residual_image[ny * width + nx1]);
                         }
                         if (nx2 >= 0 && nx2 < width) {
-                            min_val = std::min(min_val, image[ny * width + nx2]);
+                            min_val = std::min(min_val, residual_image[ny * width + nx2]);
                         }
                     }
                 }
@@ -111,6 +123,7 @@ double* errosion_dilatation(double* residual_image, int width, int height, int r
         }
     }
 
+    // Dilatation
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
             if (temp_image[y * width + x] == std::numeric_limits<double>::max()) { // Early skip
@@ -241,39 +254,38 @@ int maxHistoryLength = 5;
 
 extern "C" {
 
-    void filter_impl(uint8_t* buffer, int width, int height, int stride, int pixel_stride)
-    {
-        frame_count++;
-        CIELAB* image_lab = convert_image_to_lab((rgb*) buffer, width, height);
-        if (!first_frame_set) {
-            backgroud_image = new CIELAB[width * height];
-            memcpy(backgroud_image, image_lab, width * height * sizeof(CIELAB));
-            first_frame_set = true;
+void filter_impl(uint8_t* buffer, int width, int height, int stride, int pixel_stride)
+{
+    frame_count++;
+    CIELAB* image_lab = convert_image_to_lab((rgb*) buffer, width, height);
+    if (!first_frame_set) {
+        backgroud_image = new CIELAB[width * height];
+        memcpy(backgroud_image, image_lab, width * height * sizeof(CIELAB));
+        first_frame_set = true;
 
-            // Initialize pixel histories
-            pixelHistories.resize(width * height);
-        }
-        else {
-            refresh_background_median(backgroud_image, image_lab, width, height, pixelHistories, maxHistoryLength);
-            double* residual_image = compute_residual_image(backgroud_image, image_lab, width, height);
-            double* errosion_dilatation_image = errosion_dilatation(residual_image, width, height, 3);
-            int* hysteresis_image = hysteresis(errosion_dilatation_image, width, height, 4, 30);
+        // Initialize pixel histories
+        pixelHistories.resize(width * height);
+    }
+    else {
+        refresh_background_median(backgroud_image, image_lab, width, height, pixelHistories, maxHistoryLength);
+        double* residual_image = compute_residual_image(backgroud_image, image_lab, width, height);
+        double* erosion_dilatation_image = erosion_dilatation(residual_image, width, height, 3);
+        int* hysteresis_image = hysteresis(erosion_dilatation_image, width, height, 4, 30);
 
-            for (int y = 0; y < height; ++y)
+        for (int y = 0; y < height; ++y)
+        {
+            rgb* lineptr = (rgb*) (buffer + y * stride);
+            for (int x = 0; x < width; ++x)
             {
-                rgb* lineptr = (rgb*) (buffer + y * stride);
-                for (int x = 0; x < width; ++x)
-                {
-                    if (hysteresis_image[y*width + x] == 1) {
-                        lineptr[x].r = 255;
-                    }
+                if (hysteresis_image[y*width + x] == 1) {
+                    lineptr[x].r = 255;
                 }
             }
-            delete[] residual_image;
-            delete[] errosion_dilatation_image;
-            delete[] hysteresis_image;
         }
-        delete[] image_lab;
+        delete[] residual_image;
+        delete[] erosion_dilatation_image;
+        delete[] hysteresis_image;
     }
+    delete[] image_lab;
 }
-
+}
